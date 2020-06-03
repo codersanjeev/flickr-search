7e8a2aa45a5d1413f14b09dcc940176079a25c4c
Sanjeev <thedevelopersanjeev@gmail.com>
2020-06-03 21:08:57 +0530

    testing github actions

--- app/src/main/java/com/droidman/flickrsearch/FlickrMainActivity.kt
+++ app/src/main/java/com/droidman/flickrsearch/FlickrMainActivity.kt
@@ -30,7 +30,7 @@ class FlickrMainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private var flickrImagesAdapter = FlickrImagesAdapter()
     private var flickrImagesRepository = FlickrImagesRepository()
 
-    private var flickrQueryString = ""
+    private var flickrQueryString = "default query"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
